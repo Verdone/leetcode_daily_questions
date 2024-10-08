eba6854b7a909878a1eb2b96ee2edc4bab00326b
Giuliano <giulianoverdone@gmail.com>
2024-10-08 18:54:46 -0400

    Create 1963_minimum_number_of_swaps_to_make_the_string_balanced.java

---
+++ 1963_minimum_number_of_swaps_to_make_the_string_balanced.java
@@ -0,0 +1,17 @@
+// 1963. Minimum Number of Swaps to Make the String Balanced
+// Difficulty: Medium
+// Topics: String
+
+class Solution {
+    public int minSwaps(String s) {
+        int size = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '[') 
+                size++;
+            else if (size > 0) 
+                size--;
+        }
+        return (size + 1) / 2;
+    }
+}
