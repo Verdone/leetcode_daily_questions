f64f359090fb13a6ed3600a446b0bb0a2350e06c
Giuliano <giulianoverdone@gmail.com>
2024-11-02 15:30:01 -0400

    Create 2490_circular_sentence.py

---
+++ 2490_circular_sentence.py
@@ -0,0 +1,20 @@
+# 2490. Circular Sentence
+# Difficulty: Easy
+# Topics: String
+
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+    
+        n = len(sentence)
+        
+        if sentence[0] != sentence[n-1]:
+            return False
+            
+        for i in range(1, n-1):
+            
+            if sentence[i] == ' ':
+
+                if sentence[i-1] != sentence[i+1]:
+                    return False
+
+        return True
