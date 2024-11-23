52a0f0fdb411bd3d25b752851f3ec928634b36ce
Giuliano <giulianoverdone@gmail.com>
2024-11-23 09:48:10 -0500

    Create 1861_rotating_the_box.py

---
+++ 1861_rotating_the_box.py
@@ -0,0 +1,22 @@
+# 1861. Rotating the Box
+# Difficulty: Medium
+# Topics: Array, Matrix, Two Pointers
+
+class Solution:
+    def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
+        
+        n = len(box[0])-1
+        
+        for row in box:
+
+            i,j = n,n             
+            while i >= 0:
+                
+                if row[i] == '#':
+                    if i < j: row[i], row[j] = '.', '#'                 
+                    j -= 1
+
+                elif row[i] == '*': j = i - 1 
+                i -= 1
+
+        return  map(list, zip(*(box[::-1])))
