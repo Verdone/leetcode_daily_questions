d25a51a5a4a97e360e46152c56ba527929ee329e
Giuliano <giulianoverdone@gmail.com>
2025-02-10 16:42:20 -0500

    Create 3174_clear_digits.py

---
+++ 3174_clear_digits.py
@@ -0,0 +1,13 @@
+# 3174. Clear Digits
+# Difficulty: Easy
+# Topic: Stack
+
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        stk = []
+        for c in s:
+            if c.isdigit():
+                stk.pop()
+            else:
+                stk.append(c)
+        return "".join(stk)
